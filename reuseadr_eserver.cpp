9bce0083e4a5abc86ef882af335f46adfdf06189
Hyeungeun Kim <hyeunkim@c8r5s11.42seoul.kr>
2024-08-15 15:42:53 +0900

    dump

--- reuseadr_eserver.cpp
+++ reuseadr_eserver.cpp
@@ -6,9 +6,10 @@
 #include <sys/socket.h>
 #include <string>
 
-#define BUF_SIZE 1024
+#define TRUE 1
+#define FALSE 0
 
-void	error_handling(const std::string& msg) {
+void	error_handling(std::string msg) {
 	fputs(msg.c_str(), stderr);
 	fputc('\n', stderr);
 	exit(1);
@@ -16,12 +17,10 @@ void	error_handling(const std::string& msg) {
 
 int main(int argc, char** argv) {
 	int serv_sock, clnt_sock;
-	std::string	msg(BUF_SIZE, 0);
-	int str_len, i;
-
+	char msg[30];
+	int option, str_len;
+	socklen_t optlen, clnt_adr_sz;
 	struct sockaddr_in serv_adr, clnt_adr;
-	socklen_t clnt_adr_sz;
-
 	if (argc != 2) {
 		printf("Usage: %s <port> \n", argv[0]);
 		exit(1);
@@ -30,30 +29,30 @@ int main(int argc, char** argv) {
 	serv_sock = socket(PF_INET, SOCK_STREAM, 0);
 	if (serv_sock == -1)
 		error_handling("socket() error");
+
+	/* time-wait 상태가 되지 않도록 설정하는 옵션으로, 종료 후 바로 해당 포트를 재사용하게 해줌
+	optlen = sizeof(option);
+	option = TRUE;
+	secsocketopt(serv_sock, SOL_SOCKET, SO_REUSEADDR, (void*)&option, optlen);
+	*/
+
 	memset(&serv_adr, 0, sizeof(serv_adr));
 	serv_adr.sin_family = AF_INET;
 	serv_adr.sin_addr.s_addr = htonl(INADDR_ANY);
 	serv_adr.sin_port = htons(atoi(argv[1]));
 
-	if (bind(serv_sock, (struct sockaddr*) &serv_adr, sizeof(serv_adr)) == -1)
+	if (bind(serv_sock, (struct sockaddr*)&serv_adr, sizeof(serv_adr)))
 		error_handling("bind() error");
-
 	if (listen(serv_sock, 5) == -1)
 		error_handling("listen() error");
 	clnt_adr_sz = sizeof(clnt_adr);
+	clnt_sock = accept(serv_sock, (struct sockaddr*)&clnt_adr, &clnt_adr_sz);
 
-	for (i = 0; i < 5; i++) {
-		clnt_sock = accept(serv_sock, (struct sockaddr*) &clnt_adr, &clnt_adr_sz);
-		if (clnt_sock == -1)
-			error_handling("accept() error");
-		else
-			printf("Connected client %d\n", i + 1);
-
-		while ((str_len = read(clnt_sock, (void *)(msg.c_str()), BUF_SIZE)) != 0)
-			write(clnt_sock, msg.c_str(), str_len);
-
-		close(clnt_sock);
+	while((str_len = read(clnt_sock, msg, sizeof(msg))) != 0) {
+		write(clnt_sock, msg, str_len);
+		write(!1, msg, str_len);
 	}
+	close(clnt_sock);
 	close(serv_sock);
 	return 0;
 }
