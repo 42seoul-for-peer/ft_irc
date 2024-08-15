4b0bc6c376a8ddbdde7401c58aa5175797605557
Sumin Lee <suminn0707@gmail.com>
2024-08-15 20:53:43 +0900

    feat: select 함수 적용

--- test_server.cpp
+++ test_server.cpp
@@ -1,7 +1,13 @@
+#include <sys/_types/_socklen_t.h>
 #include <unistd.h>
 #include <iostream>
 #include <sys/socket.h>
 #include <netinet/in.h>
+
+void error_msg(std::string msg) {
+	std::cerr << msg << std::endl;
+	exit(1);
+}
 
 int main(int argc, char** argv) {
 
@@ -13,8 +19,11 @@ int main(int argc, char** argv) {
 
 	int serv_sock, clnt_sock;
 	struct sockaddr_in serv_addr, clnt_addr;
-	unsigned int len;
-	int buf_len;
+	struct timeval timeout;
+	fd_set reads, cpy_reads;
+
+	socklen_t adr_sz;
+	int buf_len, fd_max, fd_num;
 
 	char rcvBuffer[BUFSIZ];
 
@@ -24,27 +33,51 @@ int main(int argc, char** argv) {
 	serv_addr.sin_family = AF_INET;
 	serv_addr.sin_port = htons(port);
 
-	if (bind(serv_sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
-		std::cout << "bind error!" << std::endl;
-		return 1;
-	}
+	if (bind(serv_sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+		error_msg("bind error!");
+	if (listen(serv_sock, 5) < 0)
+		error_msg("listen error!");
 
-	if (listen(serv_sock, 5) < 0) {
-		std::cout << "listen error!" << std::endl;
-		return 1;
-	}
+	FD_ZERO(&reads);
+	FD_SET(serv_sock, &reads);
+	fd_max = serv_sock;
 
 	while (1) {
-		len = sizeof(clnt_addr);
-		clnt_sock = accept(serv_sock, (struct sockaddr *) &clnt_addr, &len);
+		cpy_reads = reads;
+		timeout.tv_sec = 5;
+		timeout.tv_usec = 5000;
 
-		buf_len = read(clnt_sock, rcvBuffer, sizeof(rcvBuffer));
-		rcvBuffer[buf_len] = '\0';
-		write(clnt_sock, rcvBuffer, buf_len);
+		fd_num = select(fd_max + 1, &cpy_reads, 0, 0, &timeout);
+		if (fd_num < 0)
+			break;
+		if (fd_num == 0)
+			continue;
 
-		close (clnt_sock);
+		for (int i = 0; i < fd_max + 1; i++) {
+			if (FD_ISSET(i, &cpy_reads)) {
+				if (i == serv_sock) {
+					adr_sz = sizeof(clnt_addr);
+					clnt_sock = accept(serv_sock, (struct sockaddr *) &clnt_addr, &adr_sz);
+					
+					FD_SET(clnt_sock, &reads);
+					if (fd_max < clnt_sock)
+						fd_max = clnt_sock;
+					std::cout << "connected client: " << clnt_sock << std::endl;
+				} else {
+					buf_len = read(clnt_sock, rcvBuffer, sizeof(rcvBuffer));
+					if (buf_len == 0) {
+						FD_CLR(i, &reads);
+						close(i);
+						std::cout << "closed client: " << i << std::endl;
+					} else {
+						write(i, rcvBuffer, buf_len);
+					}
+				}
+			}
+		}
 	}
 
 	close (serv_sock);
+	return 0;
 
 }
