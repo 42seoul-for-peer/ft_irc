82632690f8efc1b1182a517f7a62db972f79ccb0
Sumin Lee <suminn0707@gmail.com>
2024-08-17 18:39:13 +0900

    feat: select로 다시 복귀, read의 fd 부분을 clnt_sock에서 인덱스 값으로 수정

--- test_server.cpp
+++ test_server.cpp
@@ -2,10 +2,7 @@
 #include <unistd.h>
 #include <iostream>
 #include <sys/socket.h>
-#include <sys/event.h>
 #include <netinet/in.h>
-
-#define KQ_SIZE 100
 
 void error_msg(std::string msg) {
 	std::cerr << msg << std::endl;
@@ -22,16 +19,13 @@ int main(int argc, char** argv) {
 
 	int serv_sock, clnt_sock;
 	struct sockaddr_in serv_addr, clnt_addr;
+	struct timeval timeout;
+	fd_set reads, cpy_reads;
 
 	socklen_t adr_sz;
-	int buf_len;
+	int buf_len, fd_max, fd_num;
 
 	char rcvBuffer[BUFSIZ];
-
-	struct kevent* kq_events;
-	struct kevent event;
-
-	int kq, result, event_cnt;
 
 	serv_sock = socket(PF_INET, SOCK_STREAM, 0);
 	memset(&serv_addr, 0, sizeof(serv_addr));
@@ -44,54 +38,46 @@ int main(int argc, char** argv) {
 	if (listen(serv_sock, 5) < 0)
 		error_msg("listen error!");
 
-	kq = kqueue();
-	if (kq < 0)
-		error_msg("kqueue error!");
-	kq_events = (struct kevent *)malloc(sizeof(struct kevent) * KQ_SIZE);
-
-	EV_SET(&event, serv_sock, EVFILT_READ, EV_ADD, 0, 0, NULL);
-	result = kevent(kq, &event, 1, NULL, 0, NULL);
-	if (result < 0)
-		error_msg("kevent error!");
+	FD_ZERO(&reads);
+	FD_SET(serv_sock, &reads);
+	fd_max = serv_sock;
 
 	while (1) {
-		
-		event_cnt = kevent(kq, NULL, 0, kq_events, 1, NULL);
-		if (event_cnt < 0) {
-			std::cout << "kevent error!" << std::endl;
+		cpy_reads = reads;
+		timeout.tv_sec = 5;
+		timeout.tv_usec = 5000;
+
+		fd_num = select(fd_max + 1, &cpy_reads, 0, 0, &timeout);
+		if (fd_num < 0)
 			break;
-		}
+		if (fd_num == 0)
+			continue;
 
-		for (int i = 0; i < event_cnt; i++) {
-			if (kq_events[i].ident == serv_sock) {
-				adr_sz = sizeof(clnt_addr);
-				clnt_sock = accept(serv_sock, (struct sockaddr *) &clnt_addr, &adr_sz);
-				
-				EV_SET(&event, clnt_sock, EVFILT_READ, EV_ADD, 0, 0, NULL);
-				result = kevent(kq, &event, 1, NULL, 0, NULL);
-				if (result < 0)
-					break ;
-
-				std::cout << "connected client: " << clnt_sock << std::endl;
-			} else {
-				buf_len = read(kq_events[i].ident, rcvBuffer, sizeof(rcvBuffer));
-				if (buf_len == 0) {
-					EV_SET(&event, kq_events[i].ident, EVFILT_READ, EV_DELETE, 0, 0, NULL);
-					result = kevent(kq, &event, 1, NULL, 0, NULL);
-					close(kq_events[i].ident);
-					if (result < 0)
-						break ;
-				
-					std::cout << "closed client: " << kq_events[i].ident << std::endl;
+		for (int i = 0; i < fd_max + 1; i++) {
+			if (FD_ISSET(i, &cpy_reads)) {
+				if (i == serv_sock) {
+					adr_sz = sizeof(clnt_addr);
+					clnt_sock = accept(serv_sock, (struct sockaddr *) &clnt_addr, &adr_sz);
+					
+					FD_SET(clnt_sock, &reads);
+					if (fd_max < clnt_sock)
+						fd_max = clnt_sock;
+					std::cout << "connected client: " << clnt_sock << std::endl;
 				} else {
-					write(kq_events[i].ident, rcvBuffer, buf_len);
+					buf_len = read(i, rcvBuffer, sizeof(rcvBuffer));
+					if (buf_len == 0) {
+						FD_CLR(i, &reads);
+						close(i);
+						std::cout << "closed client: " << i << std::endl;
+					} else {
+						write(i, rcvBuffer, buf_len);
+					}
 				}
 			}
 		}
 	}
-	free(kq_events);
+
 	close (serv_sock);
-	close (kq);
 	return 0;
 
 }
