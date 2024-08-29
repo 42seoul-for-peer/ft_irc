8b9ffd8c24dd2103a2f00e4b3cbd8f235c576af9
Seungjun Lee <seungjun@c1r6s7.42seoul.kr>
2024-08-29 15:24:57 +0900

    feat: fixed structure and add functions of getter/setter
    Channel 클래스 초안

--- incs/Channel.hpp
+++ incs/Channel.hpp
@@ -9,27 +9,41 @@
 class Channel {
 // OCCF
  public:
-	// 최초로 join 사용했을 때 이름, 사용자
-	Channel(const std::string& name, Client& client);
+	Channel(std::string title);
 	~Channel();
  private:
 	Channel();
 	Channel(const Channel& copy);
 	Channel& operator = (const Channel& copy);
+
 // MEMBER VARIABLE
  private:
-	// int: operator 여부, client 정보
-	std::vector< std::pair<bool, Client&> >	_users;
-	std::string				_name;
-	std::string				_topic;
-	std::string				_password;
-	int						_max_user;
-		// default -1 (overflow 조심) mode flag 보고 set하기
-	int						_mode;
-	std::vector< std::string > _invited_user; // 명령어 동작 한 번 확인하고 필요한지 검토
+	std::vector< std::pair<bool, Client&> > _clients;
+    std::vector< std::string >              _invited_clients;
+	std::string	_title;
+	std::string	_topic;
+	std::string	_passwd;
+    int			_max_clients;
+	int			_mode;
 
 // MEMBER FUNCITON
+ public:
+	// SETTER
+	void setTopic(std::string& str);
+	void setPasswd(std::string& str);
+	void setMaxClients(int& num);
+	void setMode(int& bitNum);
 
+	// GETTER
+	const std::string& getTopic(void) const;
+	const std::string& getPasswd(void) const;
+	const int& getMaxClients(void) const;
+	const int& getMode(void) const;
+	
+	// HANDLING CLIENT
+	void addClient(Client& client);
+	void leaveClient(Client& client);
+	void addInvitedClient(std::string& name);
 };
 
 #endif
--- srcs/Channel.cpp
+++ srcs/Channel.cpp
@@ -22,3 +22,73 @@ Channel& Channel::operator = (const Channel& copy) {
 }
 
 // MEMBER FUNCTION
+Channel::Channel(std::string title)
+    : _title(title), _max_clients(-1), _mode(0)
+{
+    
+}
+
+void Channel::setTopic(std::string& str)
+{
+    _topic = str;
+}
+
+void Channel::setPasswd(std::string& str)
+{
+    _passwd = str;
+}
+
+void Channel::setMaxClients(int& num)
+{
+    _max_clients = num;
+}
+
+void Channel::setMode(int& bitNum)
+{
+    _mode = bitNum;
+}
+
+const std::string& Channel::getTopic(void) const
+{
+    return (_topic);
+}
+
+const std::string& Channel::getPasswd(void) const
+{
+    return (_passwd);
+}
+
+const int& Channel::getMaxClients(void) const
+{
+    return (_max_clients);
+}
+
+const int& Channel::getMode(void) const
+{
+    return (_mode);
+}
+
+void Channel::addClient(Client& client)
+{
+    _clients.push_back(std::pair<bool, Client&>(false, client));
+}
+
+void Channel::leaveClient(Client& client)
+{
+    std::vector< std::pair<bool, Client&> >::iterator it = _clients.begin();
+
+    while (it != _clients.end())
+    {
+        if (&(it->second) == &client)
+            break;
+        it++;
+    }
+    if (it != _clients.end())
+        _clients.erase(it);
+}
+
+void Channel::addInvitedClient(std::string& name)
+{
+    if (std::find(_invited_clients.begin(), _invited_clients.end(), name) != _invited_clients.end())
+        _invited_clients.push_back(name);
+}
