61194830489db105d28794f4350015e26101bb92
Seungjun Lee <seungjun@c5r4s7.42seoul.kr>
2024-09-02 17:31:33 +0900

    style: changed variable format of function
    매개변수 변경에 따른 함수 수정

--- srcs/command/join.cpp
+++ srcs/command/join.cpp
@@ -1,8 +1,22 @@
 #include "Command.hpp"
 
-void Command::join(int clnt_fd, Server& serv)
+/*
+? 해야 하는 것
+! 처리해야 하는 오류
+* (1) ERR_NEEDMOREPARAMS
+* (2) ERR_BANNEDFROMCHAN
+* (3) ERR_INVITEONLYCHAN
+* (4) ERR_BADCHANNELKEY
+* (5) ERR_CHANNELISFULL
+* (6) ERR_BADCHANMASK
+* (7) ERR_NOSUCHCHANNEL
+* (8) ERR_TOOMANYCHANNELS
+* (9) RPL_TOPIC
+*/
+
+void Command::join(Client& send_clnt, Server& serv)
 {
     
-    (void) clnt_fd;
+    (void) send_clnt;
     (void) serv;
 }
