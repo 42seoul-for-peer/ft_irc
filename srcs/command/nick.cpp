d7a218946b16aa46b54ec480be3aef3d1b55ea60
Hyeungeun Kim <hyeunkim@c8r5s11.42seoul.kr>
2024-09-02 17:51:38 +0900

    refactor: 함수 인자 변경

--- srcs/command/nick.cpp
+++ srcs/command/nick.cpp
@@ -13,39 +13,44 @@
 // 우린 멀티 서버 아니라 불필요
 // ERR_NICKCOLLISION: 서버를 타고 갔을 때 다른 client와 겹치는 경우
 
-void	Command::nick(int clnt_fd, Server& serv) {
+// invalid character
+// ~!@#$%&*()_+-=
+# define INVALID_CHARS "~!@#$%&*()_+-="
+
+void	Command::nick(Client& send_clnt, Server& serv) {
 	// 비밀번호 있는지 확인해야함
-	std::map< int, Client* >::const_iterator it = serv.getClients().find(clnt_fd);
-	if (it == serv.getClients().end() || it->second->getIsRegistered() == false) {
-		_rpl_no = ERR_NOTREGISTERED;
-		_proto_msg = ":You have not registerd";
-		std::cout << _proto_msg << std::endl;
+	if (send_clnt.getIsRegistered() == false) {
+		_receiver.push(make_pair(send_clnt.getNickname(), ERR_NOTREGISTERED));
+		_msg = "You have not registerd";
 		return ;
 	}
+	// 비밀번호가 같이 안 들어온 경우
 	if (_args.size() < 1) {
-		_rpl_no = ERR_NONICKNAMEGIVEN;
-		_proto_msg = ":No nickname given";
+		_receiver.push(make_pair(send_clnt.getNickname(), ERR_NONICKNAMEGIVEN));
+		_msg = "No nickname given";
 		return ;
 	}
-	// 인자가 한 개보다 많은 경우는 무시되나? (프로토콜 뜯어보기)
+	// nickname으로 쓸 수 없는 문자 들어있는지 검사
+	std::string invalid_chars("~!@#$%&*()_+-=");
 	std::string	newNick =_args.front();
 	_args.pop();
 	int	new_nick_len = newNick.length();
 	for (int i = 0; i < new_nick_len; i++) {
-		if (newNick[i] == '#') {
-			_rpl_no = ERR_ERRONEUSNICKNAME;
-			_proto_msg = newNick + ":Erroneus nickname";
+		if (invalid_chars.find(newNick[i])) {
+			_receiver.push(make_pair(send_clnt.getNickname(), ERR_ERRONEUSNICKNAME));
+			_msg = "Erroneus nickname";
 			return ;
 		}
 	}
 	// 중복되지 않는지 확인하는 상황
+	std::map< int, Client* >::const_iterator it = serv.getClients().begin();
 	while (it != serv.getClients().end()) {
 		if (it->second->getNickname() == newNick) {
-			_rpl_no = ERR_NICKNAMEINUSE;
-			_proto_msg = newNick + ":Nickname is already in use";
+			_receiver.push(make_pair(send_clnt.getNickname(), ERR_NICKNAMEINUSE));
+			_msg = "Nickname is already in use";
 			return ;
 		}
 		it++;
 	}
-	it->second->setNickname(newNick);
+	send_clnt.setNickname(newNick);
 }
