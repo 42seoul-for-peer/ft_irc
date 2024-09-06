5f7029d6d6a30b13add82c5c43f0d247137b53ef
Seungjun Lee <seungjun@c1r6s7.42seoul.kr>
2024-09-06 17:13:02 +0900

    feat: add i,t,l flag functions and fix structure of MODE

--- srcs/command/mode.cpp
+++ srcs/command/mode.cpp
@@ -28,15 +28,15 @@ O  1| ERR_NEEDMOREPARAMS    |461| "<command> :Not enough parameters"
 O  2| ERR_CHANOPRIVSNEEDED  |482| "<channel> :You're not channel operator"
 O  3| ERR_NOTONCHANNEL      |442| "<channel> :You're not on that channel"
 O  4| ERR_NOSUCHCHANNEL     |403| "<channel name> :No such channel"
-*  5| ERR_KEYSET            |467| "<channel> :Channel key already set"
-*  6| ERR_UNKNOWNMODE       |472| "<char> :is unknown mode char to me"
+O  5| ERR_KEYSET            |467| "<channel> :Channel key already set"
+O  6| ERR_UNKNOWNMODE       |472| "<char> :is unknown mode char to me"
 // 7| ERR_NOSUCHNICK        |401| "<nickname> :No such nick/channel" (user는 관리하지 않음)
 // 8| ERR_USERSDONTMATCH    |502| ":Cant change mode for other users" (user는 관리하지 않음)
 // 9| ERR_UMODEUNKNOWNFLAG  |501| ":Unknown MODE flag" (user는 관리하지 않음)
-* 10| RPL_CHANNELMODEIS     |324| "<channel> <mode> <mode params>"
+O 10| RPL_CHANNELMODEIS     |324| "<channel> <mode> <mode params>"
 // 11| RPL_BANLIST           |367| "<channel> <banid>" (ban은 구현 범위가 아님)
 // 12| RPL_ENDOFBANLIST      |368| "<channel> :End of channel ban list" (ban은 구현 범위가 아님)
-* 13| RPL_UMODEIS           |221| "<user mode string>"
+// 13| RPL_UMODEIS           |221| "<user mode string>" (user는 관리하지 않음)
 To answer a query about a client's own mode, RPL_UMODEIS is sent back.
 */
 
@@ -64,11 +64,16 @@ std::queue< std::pair< bool, char > > getFlag(const std::string& string)
 
 void Command::mode(Client& send_clnt, Server& serv)
 {
+    std::string pref;
+    std::string msg;
     // 인자가 부족함 (최소 1개의 인자 필요 <channel>)
     if (_args.size() < 1)
     {
         //! 1| ERR_NEEDMOREPARAMS |461| "<command> :Not enough parameters"
-        _receiver.insert(make_pair(send_clnt.getNickname(), ERR_NEEDMOREPARAMS));
+        pref = serv.generatePrefix(send_clnt.getNickname(), ERR_NEEDMOREPARAMS, send_clnt.getSockFd());
+		msg = _genProtoMsg(ERR_NEEDMOREPARAMS, pref, send_clnt.getNickname(), "MODE");
+		setMsg(send_clnt.getNickname(), msg);
+        // _receiver.insert(make_pair(send_clnt.getNickname(), ERR_NEEDMOREPARAMS));
         return ;
     }
     std::map< std::string, Channel* >::const_iterator chan_it = serv.getChannels().find(_args.front());
@@ -77,7 +82,10 @@ void Command::mode(Client& send_clnt, Server& serv)
     if (chan_it == serv.getChannels().end())
     {
         //! 4| ERR_NOSUCHCHANNEL |403| "<channel name> :No such channel"
-        _receiver.insert(make_pair(send_clnt.getNickname(), ERR_NOSUCHCHANNEL));
+        pref = serv.generatePrefix(send_clnt.getNickname(), ERR_NOSUCHCHANNEL);
+        msg = _genProtoMsg(ERR_NOSUCHCHANNEL, pref, send_clnt.getNickname(), chan_it->first);
+        setMsg(send_clnt.getNickname(), msg);
+        // _receiver.insert(make_pair(send_clnt.getNickname(), ERR_NOSUCHCHANNEL));
         return ;
     }
     std::vector< std::pair< bool, Client* > >::const_iterator chan_clnt_it = \
@@ -88,7 +96,10 @@ void Command::mode(Client& send_clnt, Server& serv)
         if (chan_clnt_it == chan_it->second->getClients().end())
         {
             //! 3| ERR_NOTONCHANNEL |442| "<channel> :You're not on that channel"
-            _receiver.insert(make_pair(send_clnt.getNickname(), ERR_NOTONCHANNEL));
+            pref = serv.generatePrefix(send_clnt.getNickname(), ERR_NOTONCHANNEL);
+            msg = _genProtoMsg(ERR_NOTONCHANNEL, pref, send_clnt.getNickname(), chan_it->first);
+            setMsg(send_clnt.getNickname(), msg);
+            // _receiver.insert(make_pair(send_clnt.getNickname(), ERR_NOTONCHANNEL));
             return ;
         }
         if (chan_clnt_it->second->getNickname() == send_clnt.getNickname())
@@ -97,7 +108,10 @@ void Command::mode(Client& send_clnt, Server& serv)
             if (chan_clnt_it->first != true)
             {
                 //! 2| ERR_CHANOPRIVSNEEDED |482| "<channel> :You're not channel operator"
-                _receiver.insert(make_pair(send_clnt.getNickname(), ERR_CHANOPRIVSNEEDED));
+                pref = serv.generatePrefix(send_clnt.getNickname(), ERR_CHANOPRIVSNEEDED);
+                msg = _genProtoMsg(ERR_CHANOPRIVSNEEDED, pref, send_clnt.getNickname(), chan_it->first);
+                setMsg(send_clnt.getNickname(), msg);
+                // _receiver.insert(make_pair(send_clnt.getNickname(), ERR_CHANOPRIVSNEEDED));
                 return ;
             }
             else
@@ -110,13 +124,18 @@ void Command::mode(Client& send_clnt, Server& serv)
     while (!flag_queue.empty())
     {
         std::pair< bool, char > flag = flag_queue.front();
+        // flag가 'k' 옵션
         if (flag.second == 'k')
         {
             // 'k' 옵션은 args가 필수인데, args가 비어있음
+            // 이 에러는 원래 696 error code인데, rfc에 존재하지 않아서 임의로 지정한 메시지를 보냄
             if (_args.empty())
             {
                 //! 1| ERR_NEEDMOREPARAMS |461| "<command> :Not enough parameters"
-                _receiver.insert(make_pair(send_clnt.getNickname(), ERR_NEEDMOREPARAMS));
+                pref = serv.generatePrefix(send_clnt.getNickname(), ERR_CHANOPRIVSNEEDED);
+                msg = _genProtoMsg(ERR_CHANOPRIVSNEEDED, pref, send_clnt.getNickname(), chan_it->first, "k");
+                setMsg(send_clnt.getNickname(), msg);
+                // _receiver.insert(make_pair(send_clnt.getNickname(), ERR_NEEDMOREPARAMS));
                 continue ;
             }
             std::string pw_str = _args.front();
@@ -125,7 +144,10 @@ void Command::mode(Client& send_clnt, Server& serv)
             {
                 chan_it->second->setMode(flag.first, MODE_K);
                 chan_it->second->setPasswd(pw_str);
-                _receiver.insert(make_pair(chan_it->first, RPL_CHANNELMODEIS));
+                pref = serv.generatePrefix(send_clnt.getNickname(), 0);
+                msg = _genProtoMsg(0, pref, "MODE", chan_it->first, "+k", ":" + pw_str);
+                setMsg(chan_it->first, msg);
+                // _receiver.insert(make_pair(chan_it->first, RPL_CHANNELMODEIS));
             }
             // flag가 flase(-)이고, 이미 -k 모드가 아닐 때
             else if(flag.first == false && (chan_it->second->getMode() & MODE_K))
@@ -134,23 +156,115 @@ void Command::mode(Client& send_clnt, Server& serv)
                 if (chan_it->second->getPasswd() == pw_str)
                 {
                     chan_it->second->setMode(false, MODE_K);
-                    _receiver.insert(make_pair(chan_it->first, RPL_CHANNELMODEIS));
+                    pref = serv.generatePrefix(send_clnt.getNickname(), 0);
+                    msg = _genProtoMsg(0, pref, "MODE", chan_it->first, "-k", ":" + pw_str);
+                    setMsg(chan_it->first, msg);
+                    // _receiver.insert(make_pair(chan_it->first, RPL_CHANNELMODEIS));
                 }
                 // 인자의 string과 설정된 비밀번호가 다를 때
                 else
                 {
                     //! 5| ERR_KEYSET |467| "<channel> :Channel key already set"
-                    _receiver.insert(make_pair(send_clnt.getNickname(), ERR_KEYSET));
+                    pref = serv.generatePrefix(send_clnt.getNickname(), ERR_KEYSET);
+                    msg = _genProtoMsg(ERR_KEYSET, pref, send_clnt.getNickname(), chan_it->first);
+                    setMsg(send_clnt.getNickname(), msg);
+                    // _receiver.insert(make_pair(send_clnt.getNickname(), ERR_KEYSET));
                 }
             }
             _args.pop();
+        }
+        // flag가 'l'
+        else if (flag.second == 'l')
+        {
+            // flag가 '-'
+            if (flag.first == false && (chan_it->second->getMode() & MODE_L))
+            {
+                chan_it->second->setMode(flag.first, MODE_L);
+                pref = serv.generatePrefix(send_clnt.getNickname(), 0);
+                msg = _genProtoMsg(0, pref, "MODE", chan_it->first, ":-l");
+                setMsg(chan_it->first, msg);
+                // _receiver.insert(make_pair(send_clnt.getNickname(), ERR_KEYSET));
+            }
+            else if (flag.first == true)
+            {
+                if (_args.empty())
+                {
+                    //! 1| ERR_NEEDMOREPARAMS |461| "<command> :Not enough parameters"
+                    pref = serv.generatePrefix(send_clnt.getNickname(), ERR_NEEDMOREPARAMS);
+                    msg = _genProtoMsg(ERR_NEEDMOREPARAMS, pref, send_clnt.getNickname(), chan_it->first, "MODE");
+                    setMsg(send_clnt.getNickname(), msg);
+                    // _receiver.insert(make_pair(send_clnt.getNickname(), ERR_NEEDMOREPARAMS));
+                    continue ;
+                }
+                int limit_str = std::atoi(_args.front().c_str());
+                if (limit_str < 0)
+                    limit_str = 0;
+                // flag가 +고, 현재 채널의 최대 유저수와 바꾸려는 값이 다름
+                if (chan_it->second->getMaxClients() != limit_str)
+                {
+                    chan_it->second->setMode(flag.first, MODE_L);
+                    chan_it->second->setMaxClients(limit_str);
+                    pref = serv.generatePrefix(send_clnt.getNickname(), 0);
+                    msg = _genProtoMsg(0, pref, "MODE", chan_it->first, "+l", ":" + _args.front());
+                    setMsg(chan_it->first, msg);
+                }
+                _args.pop();
+            }
+        }
+        // flag가 'o'
+        else if (flag.second == 'o')
+        {
+            ;
+        }
+        // flag가 'i'
+        else if (flag.second == 'i')
+        {
+            // flag가 '+', 채널은 '-'인 상태 / 또는 flag가 '-', 채널은 '+'인 상태
+            if (flag.first == true && !(chan_it->second->getMode() & MODE_I) || \
+                    flag.first == false && (chan_it->second->getMode() & MODE_I))
+            {
+                chan_it->second->setMode(flag.first, MODE_I);
+                pref = serv.generatePrefix(send_clnt.getNickname(), 0);
+                if (flag.first == true)
+                    msg = _genProtoMsg(0, pref, "MODE", chan_it->first, ":+i");
+                else
+                    msg = _genProtoMsg(0, pref, "MODE", chan_it->first, ":-i");
+                setMsg(chan_it->first, msg);
+                // _receiver.insert(make_pair(chan_it->first, RPL_CHANNELMODEIS));
+            }
+        }
+        // flag가 't'
+        else if (flag.second == 't')
+        {
+            if (flag.first == true && !(chan_it->second->getMode() & MODE_T) || \
+                    flag.first == false && (chan_it->second->getMode() & MODE_T))
+            {
+                chan_it->second->setMode(flag.first, MODE_T);
+                pref = serv.generatePrefix(send_clnt.getNickname(), 0);
+                if (flag.first == true)
+                    msg = _genProtoMsg(0, pref, "MODE", chan_it->first, ":+t");
+                else
+                    msg = _genProtoMsg(0, pref, "MODE", chan_it->first, ":-t");
+                setMsg(chan_it->first, msg);
+                // _receiver.insert(make_pair(chan_it->first, RPL_CHANNELMODEIS));
+            }
         }
         // 해당 flag가 존재하지 않음
         else
         {
             //! 6| ERR_UNKNOWNMODE |472| "<char> :is unknown mode char to me"
-            _receiver.insert(make_pair(send_clnt.getNickname(), ERR_UNKNOWNMODE));
+            pref = serv.generatePrefix(send_clnt.getNickname(), ERR_UNKNOWNMODE);
+            msg = _genProtoMsg(ERR_UNKNOWNMODE, pref, send_clnt.getNickname(), std::string(flag.second));
+            setMsg(send_clnt.getNickname(), msg);
+            // _receiver.insert(make_pair(send_clnt.getNickname(), ERR_UNKNOWNMODE));
         }
         flag_queue.pop();
     }
 }
+
+/* MODE #room0 +kli-tasdf password 20
+!:irc.local 472 seungjun a :is not a recognised channel mode.
+!:irc.local 472 seungjun d :is not a recognised channel mode.
+!:irc.local 472 seungjun f :is not a recognised channel mode.
+!:seungjun!root@127.0.0.1 MODE #room0 +kli-t password :20
+*/
