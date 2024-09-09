ba561338e2bcac49209e5f0539a278bb3ac1ca33
Hyeungeun Kim <hyeunkim@c8r5s11.42seoul.kr>
2024-09-09 13:51:08 +0900

    fix: pass.cpp 수정

--- srcs/command/pass.cpp
+++ srcs/command/pass.cpp
@@ -8,23 +8,24 @@
 // ERR_PASSWDMISMATCH 464
 
 void	Command::pass(Client& send_clnt, Server& serv) {
+	std::string			prefix;
+	const std::string	send_nick = send_clnt.getNickname();
 	if (_args.size() != 1) {
-		_receiver.insert(make_pair(std::string(""), ERR_NEEDMOREPARAMS));
+		prefix = serv.generatePrefix(send_nick, ERR_NEEDMOREPARAMS);
+		setMsgs(send_nick, _genProtoMsg(ERR_NEEDMOREPARAMS, prefix));
 		return ;
 	}
 
 	if (send_clnt.getIsRegistered() == false) {
 		if (serv.getPassword() != _args.front()) {
-			_receiver.insert(make_pair(send_clnt.getNickname(), ERR_PASSWDMISMATCH));
+			prefix = serv.generatePrefix(send_nick, ERR_PASSWDMISMATCH);
+			setMsgs(send_nick, _genProtoMsg(ERR_PASSWDMISMATCH, prefix));
 			return ;
 		}
 		send_clnt.setRegistered();
 	}
 	else {
-		_receiver.insert(make_pair(send_clnt.getNickname(), ERR_ALREADYREGISTRED));
+		prefix = serv.generatePrefix(send_nick, ERR_ALREADYREGISTRED);
+		setMsgs(send_nick, _genProtoMsg(ERR_ALREADYREGISTRED, prefix));
 	}
-	// nick(send_clnt, serv);
-	// user(send_clnt, serv);
-	// _receiver.insert(make_pair(std::string(send_clnt.getNickname()), RPL_WELCOME));
-	// std::cout << "returned to pass\n" << _receiver.size() << std::endl;
 }
