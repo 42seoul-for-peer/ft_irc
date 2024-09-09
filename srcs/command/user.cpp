1c4d7b88c307a6149948248c106a1077a09d0f5c
Hyeungeun Kim <hyeunkim@c8r5s11.42seoul.kr>
2024-09-09 14:27:57 +0900

    fix: user command

--- srcs/command/user.cpp
+++ srcs/command/user.cpp
@@ -19,25 +19,27 @@
 */
 
 void	Command::user(Client& send_clnt, Server& serv) {
-	(void) serv;
-	// PASS, NICK 명령에서 문제가 발생, _receiver가 비어있지 않음
-	if (!_receiver.empty() || _args.empty())
+	std::string	prefix;
+	std::string send_nick = send_clnt.getNickname();
+	// PASS, NICK 명령에서 문제가 발생 -> _msgs에 해당 클라이언트가 있음
+	if (_msgs.find(send_nick)!=_msgs.end()){
 		return ;
+	}
 	// 클라이언트가 충분한 파라미터를 전달하지 않음
 	if (_args.size() < 4)
 	{
-		_receiver.insert(make_pair(_args.front(), ERR_NEEDMOREPARAMS));
-		// _msg = ":irc.local 461 as " + _cmd + " :Not enough parameters";
+		prefix = serv.generatePrefix(send_nick, ERR_NEEDMOREPARAMS);
+		setMsgs(send_nick, _genProtoMsg(ERR_NEEDMOREPARAMS, prefix));
 		return ;
 	}
 	// 이미 등록된 클라이언트임 (현재 판단 조건: username이 이미 존재함)
 	if (!send_clnt.getUsername().empty())
 	{
-		_receiver.insert(make_pair(_args.front(), ERR_ALREADYREGISTRED));
-		// _msg= ":irc.local 462 " + send_clnt.getNickname() + ":You may not reregister";
+		prefix = serv.generatePrefix(send_nick, ERR_ALREADYREGISTRED);
+		setMsgs(send_nick, _genProtoMsg(ERR_ALREADYREGISTRED, prefix));
 		return ;
 	}
-	std::cout << "nick in user: " << send_clnt.getNickname() << std::endl;
 	send_clnt.setUsername(_args.front());
-	_receiver.insert(make_pair(send_clnt.getNickname(), RPL_WELCOME));
+	prefix = serv.generatePrefix(send_nick, RPL_WELCOME);
+	setMsgs(send_nick, _genProtoMsg(RPL_WELCOME, prefix));
 }
