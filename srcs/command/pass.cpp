49e0e8a4f72928b37834503a064100740fc20e1b
Hyeungeun Kim <hyeunkim@c8r5s11.42seoul.kr>
2024-09-10 11:42:58 +0900

    fix: send_nick->sender(pass.cpp)

--- srcs/command/pass.cpp
+++ srcs/command/pass.cpp
@@ -7,25 +7,30 @@
 // ERR_ALREADYREGISTERED 462
 // ERR_PASSWDMISMATCH 464
 
+// 등록 전까지는 pass를 계속 받을 수 있고, update 되어야 함
+// passwd의 경우는 띄어쓰기를 포함할 수 있으니까 다 받아서 처리해야함
+
 void	Command::pass(Client& send_clnt, Server& serv) {
 	std::string			prefix;
 	const std::string	send_nick = send_clnt.getNickname();
 	if (_args.size() != 1) {
-		prefix = serv.genPrefix(send_nick, ERR_NEEDMOREPARAMS);
-		setMsgs(send_nick, _genProtoMsg(ERR_NEEDMOREPARAMS, prefix));
+		prefix = serv.genPrefix(_sender, ERR_NEEDMOREPARAMS);
+		setMsgs(_sender, _genProtoMsg(ERR_NEEDMOREPARAMS, prefix));
 		return ;
 	}
-
+	// 등록되지 않은 사용자인 경우
 	if (send_clnt.getIsRegistered() == false) {
 		if (serv.getPassword() != _args.front()) {
-			prefix = serv.genPrefix(send_nick, ERR_PASSWDMISMATCH);
-			setMsgs(send_nick, _genProtoMsg(ERR_PASSWDMISMATCH, prefix));
+			prefix = serv.genPrefix(_sender, 0);
+			setMsgs(_sender, "Wrong Password for this server.\nCONNECTION ENDED BY SERVER\n");
+			send_clnt.setConnected(false);
 			return ;
 		}
 		send_clnt.setIsRegistered();
 	}
+	// 재등록하려는 경우
 	else {
-		prefix = serv.genPrefix(send_nick, ERR_ALREADYREGISTRED);
-		setMsgs(send_nick, _genProtoMsg(ERR_ALREADYREGISTRED, prefix));
+		prefix = serv.genPrefix(_sender, ERR_ALREADYREGISTRED);
+		setMsgs(_sender, _genProtoMsg(ERR_ALREADYREGISTRED, prefix));
 	}
 }
