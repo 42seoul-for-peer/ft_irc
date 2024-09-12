70f82933f2924cb79e83f90f7cc7b9533cec185d
Seungjun Lee <seungjun@c1r6s7.42seoul.kr>
2024-09-12 16:29:55 +0900

    refactor: PART 리팩토링 및 코드 수정

--- srcs/command/part.cpp
+++ srcs/command/part.cpp
@@ -1,62 +1,48 @@
 #include "Command.hpp"
 
-/*
-ERR_NEEDMOREPARAMS
-ERR_NOSUCHCHANNEL
-ERR_NOTONCHANNEL
-*/
-
 void Command::part(Client& send_clnt, Server& serv)
 {
-	std::string prefix;
-
 	if (_args.size() < 1)
 	{
-		prefix = serv.genPrefix(_sender, ERR_NEEDMOREPARAMS);
-		setMsgs(_sender, _genProtoMsg(ERR_NEEDMOREPARAMS, prefix));
+		setMsgs(_send_nick, _genMsg(ERR_NEEDMOREPARAMS, _cmd));
 		return ;
 	}
-	std::map< std::string, Channel* >::iterator chan_it;
-	std::map< std::string, Channel* > chan_list = serv.getChannels();
-	std::vector<std::string> channels = _parsebyComma();
+
+	std::map< std::string, Channel* > 			serv_channels = serv.getChannels();
+	std::vector< std::string >					clnt_channels = send_clnt.getCurrChannel();
+	std::map< std::string, Channel* >::iterator	target;
+
+	std::vector<std::string>	chan_args = _parsebyComma();
+	const int					chan_size = chan_args.size();
 	_args.pop();
-	const std::string leaveMessage = _appendRemaining();
-	const int channel_size = channels.size();
-	for (int i = 0; i < channel_size; i++)
+	const std::string			leaveMessage = _appendRemaining();
+
+	for (int i = 0; i < chan_size; i++)
 	{
-		// 채널 먼저 찾기
-		chan_it = chan_list.find(channels[i]);
-		// 존재하지 않는 채널명
-		if (chan_it == chan_list.end())
+		target = serv_channels.find(chan_args[i]);
+		//! 존재하지 않는 채널명
+		if (target == serv_channels.end())
 		{
-			prefix = serv.genPrefix(_sender, ERR_NOSUCHCHANNEL);
-			setMsgs(_sender, _genProtoMsg(ERR_NOSUCHCHANNEL, prefix, channels[i]));
+			setMsgs(_send_nick, _genMsg(ERR_NOSUCHCHANNEL, chan_args[i]));
 			continue ;
 		}
-		// 채널은 존재하지만 클라이언트가 속해있지 않음
-		std::vector< std::string > currChannelList = send_clnt.getCurrChannel();
-		if (std::find(currChannelList.begin(), currChannelList.end(), chan_it->first) == currChannelList.end())
+		//! 채널은 존재하지만 클라이언트가 속해있지 않음
+		if (std::find(clnt_channels.begin(), clnt_channels.end(), target->first) == clnt_channels.end())
 		{
-			prefix = serv.genPrefix(_sender, ERR_NOTONCHANNEL);
-			setMsgs(_sender, _genProtoMsg(ERR_NOTONCHANNEL, prefix, channels[i]));
+			setMsgs(_send_nick, _genMsg(ERR_NOTONCHANNEL, chan_args[i]));
 			continue ;
 		}
-		// 정상 작동 상황
-		// (1) client의 current channel list에서 삭제
-		// (2) channel의 client list에서 삭제
-		// (3) channel이 비게 될 경우, server에서 channel 삭제
-		send_clnt.leaveChannel(*(chan_it->second));
-		chan_it->second->deleteClient(send_clnt);
-		if (chan_it->second->getClients().size() == 0)
-		{
-			serv.deleteChnl(chan_it->second);
-			continue ;
-		}
+		send_clnt.leaveChannel(*(target->second));
+		target->second->deleteClient(send_clnt);
+		std::string msg;
+		if (leaveMessage.empty())
+			msg = _genMsg(0, _cmd, ":" + target->first);
+		else 
+			msg = _genMsg(0, _cmd + " " + target->first, ":" + leaveMessage);
+		setMsgs(_send_nick, msg);
+		if (target->second->getClients().size() == 0)
+			serv.deleteChnl(target->second);
 		else
-		{
-			prefix = serv.genPrefix(_sender, 0);
-			std::string msg = prefix + " PART " + chan_it->first + " :" + leaveMessage + "\n";
-			setMsgs(chan_it->first, msg);
-		}
+			setMsgs(target->first, msg);
 	}
 }
