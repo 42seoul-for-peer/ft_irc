72fe5a6f237466c8a04e0dc27b30b1dec31cd72f
Seungjun Lee <seungjun@c1r6s7.42seoul.kr>
2024-09-12 12:05:00 +0900

    fix: 이미 접속중인 채널에 JOIN 가능한 오류 수정

--- srcs/command/join.cpp
+++ srcs/command/join.cpp
@@ -1,37 +1,5 @@
-
 #include "Command.hpp"
-
 /*
-TODO
-"JOIN <channel>{,<channel>} [<key>{,<key}]
-* JOIN  JOIN 명령은 클라이언트가 특정 채널을 listen하기 시작할 때 사용합니다.
-* 클라이언트가 채널에 가입할 수 있는지 여부는 클라이언트가 연결한 서버에서만 확인합니다.
-* 채널이 다른 서버로부터 수신을 받으면, 다른 모든 서버는 자동으로 사용자를 채널에 추가합니다.
-* 이에 영향을 미치는 조건은 다음과 같습니다:
-* 1. 채널이 초대 전용인 경우 사용자는 초대받아야 합니다.
-* 2. 사용자의 nickname/username/hostname는 활성화된 bans와 일치하지 않아야 합니다.
-* 3. key(password)가 설정되어 있는 경우 올바른 key(password)를 입력해야 합니다.
-* 이것들에 대해서는 MODE 명령에서 더 자세히 설명됩니다(자세한 내용은 섹션 4.2.3 참조).
-
-* 일단 user가 채널에 join하면, 서버가 수신하는 채널에 영향을 미치는 모든 명령에 대한 알림을 받습니다.
-* 여기에는 MODE, KICK, PART, QUIT, 그리고 PRIVMSG/NOTICE가 포함됩니다.
-* JOIN 명령이 성공하면 사용자는 채널의 토픽(RPL_TOPIC 사용)과 채널에 있는 사용자 목록(RPL_NAMREPLY 사용)을 보내며,
-* 여기에는 사용자가 포함되어야 합니다.
-
-todo 해야 하는 것
-? * 성공
-* 채널이 존재하지 않음
-	- 채널이 새로 생김
-	- client가 채널에 속하게 됨
-	- client가 채널의 operator가 됨
-* 채널이 존재했음
-	- 채널에 접속함(새로 생성된 것은 아님)
-	- client가 채널에 속하게 됨
-
-! * 실패/오류
-* asdf
-
-todo 처리해야 하는 오류
 O (1) ERR_NEEDMOREPARAMS<461>   -> "<command> :Not enough parameters"
 //(2) ERR_BANNEDFROMCHAN<474>   -> "<channel> :Cannot join channle (+b)" (구현 불필요)
 O (3) ERR_INVITEONLYCHAN<473>   -> "<channel> :Cannot join channel (+i)"
@@ -45,15 +13,6 @@ O (8) ERR_TOOMANYCHANNELS<405>  -> "<channel name> :You have joined too many cha
 * (10) RPL_NOTOPIC<331>         -> "<channel> :No topic is set"
 * (11) RPL_NAMREPLY<353>		-> "<channel> :[[@|+]<nick> [[@|+]<nick> [...]]]"
 * (12) RPL_ENDOFNAMES<366>		-> "<channel> :End of /NAMES list"
-To reply to a NAMES message, a reply pair consisting
-				  of RPL_NAMREPLY and RPL_ENDOFNAMES is sent by the
-				  server back to the client.  If there is no channel
-				  found as in the query, then only RPL_ENDOFNAMES is
-				  returned.  The exception to this is when a NAMES
-				  message is sent with no parameters and all visible
-				  channels and contents are sent back in a series of
-				  RPL_NAMEREPLY messages with a RPL_ENDOFNAMES to mark
-				  the end.
 
 * 채널의 이름은 '&' 또는 '#'로 시작하는 200자 이하의 string입니다.
 * 첫 번째 문자가 '&' 또는 '#' 이어야 한다는 것 외의 요구사항은
@@ -69,7 +28,6 @@ To reply to a NAMES message, a reply pair consisting
 * (2) 초대되지 않음 -> You must be invited
 * (3) 최대 접속 가능 유저 수에는 제한받지 않음 (초대 전용 서버일 경우)
 
-todo "JOIN <channel>{,<channel>} [<key>{,<key}]
 */
 
 #include <iostream>
@@ -135,6 +93,11 @@ void Command::join(Client& send_clnt, Server& serv)
 		// 채널이 존재함 (접속 시도, 비밀번호 확인 필요)
 		else
 		{
+			// 이미 접속중인 채널
+			std::cout << "존재하는 채널" << std::endl;
+			std::vector< std::string > curr_chan = send_clnt.getCurrChannel();
+			if (std::find(curr_chan.begin(), curr_chan.end(), titles[i]) != curr_chan.end())
+				continue ;
 			const int chan_mode = chan_it->second->getMode();
 			// key가 필요한 채널
 			if (chan_mode & MODE_K)
