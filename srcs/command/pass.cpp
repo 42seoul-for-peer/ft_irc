0a8d2d019dfade9ae8aebe3214d0e14c11e0084b
Hyeungeun Kim <hyeunkim@c8r5s11.42seoul.kr>
2024-09-12 14:52:47 +0900

    fix: pass쪽 genMsg로 변경

--- srcs/command/pass.cpp
+++ srcs/command/pass.cpp
@@ -11,15 +11,12 @@
 // passwd의 경우는 띄어쓰기를 포함할 수 있으니까 다 받아서 처리해야함
 
 void	Command::pass(Client& send_clnt, Server& serv) {
-	std::string	prefix;
 	std::string	passwd;
-	if (_args.size() != 1) {
-		prefix = serv.genPrefix(_sender, ERR_NEEDMOREPARAMS);
-		setMsgs(_sender, _genProtoMsg(ERR_NEEDMOREPARAMS, prefix));
+	if (_args.size() < 1) {
+		setMsgs(_send_nick, _genMsg(ERR_NEEDMOREPARAMS, _cmd));
 		return ;
 	}
-	// passwd 문자열 형태로 복원
-	// 비밀번호가 "p(공백)(공백)(공백)wd"인 경우 복원 결과가 p(공백)wd가 되는 문제가 있음
+	// passwd 문자열 형태로 복원 (공백 여러개가 들어가는 경우는 잘못되었다고 판단함)
 	while (_args.size()){
 		passwd += _args.front();
 		_args.pop();
